79336783c01b72e53acbcebb9982b3be2c395098
Akhilesh-Ojha <ojha.akhil123@gmail.com>
2020-10-14 17:17:30 +0530

    global logic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let userNameVar = "";
+let passwordVar = "";
+class App extends Component {
+
+  state = {
+    userName: '',
+    userPassword: '',
+
+  }
+
+  onUserNameChangeHandler = (event) => {
+    userNameVar = event.target.value;
+  }
+
+  onPasswordChangeHandler = (event) => {
+    passwordVar = event.target.value;
+  }
+
+  singUpHandler() {
+    sessionStorage.setItem('userDetails', JSON.stringify({userNameVar, passwordVar}));
+  }
+
+  componentDidMount() {
+    window.onbeforeunload = (event) => {
+      localStorage.setItem('userDetails', JSON.stringify({userNameVar, passwordVar}));
+      var dialogText = "Changes will be lost";
+      event.returnValue = dialogText;
+      return dialogText;
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input type="text" placeholder="UserName" onChange={this.onUserNameChangeHandler} />
+        <input type="password" placeholder="Password" onChange={this.onPasswordChangeHandler} />
+        <button onClick={this.singUpHandler}>Sign Up</button>
+      </div>
+    );
+  }
 }
 
 export default App;
+
