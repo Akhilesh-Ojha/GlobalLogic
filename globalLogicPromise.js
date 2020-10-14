d76900b15e1a050756f1a552f5dc031e8adb9aac
Akhilesh-Ojha <ojha.akhil123@gmail.com>
2020-10-14 17:21:54 +0530

    added two more files

---
+++ globalLogicPromise.js
@@ -0,0 +1,26 @@
+
+function sayName(name) {
+    return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({name: "Global Logic"})
+        }, 1000) 
+    })
+}
+
+function greet(nameOfCompany) {
+    return new Promise((resolve , reject) => {
+      setTimeout(() => {
+          resolve({variable: "Hello " + nameOfCompany.name })
+      }, 1000)  
+    })
+}
+
+
+sayName("Global Logic")
+.then(company => greet(company))
+.then(greeting => console.log(greeting));
+
+
+
+
+
